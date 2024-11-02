70d7be536fc236e99545aed27732147e93588c46
cecidavi <cecireicito_@hotmail.com>
2024-11-02 11:25:06 -0600

    Pagina web KNQ

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KNQ TRANSPORTES DE PERSONAL</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <header>
+        <h1>Bienvenidos a Empresa de Transporte De Personal Kin Q</h1>
+        <img src="asset\knqlogo.jpg" alt="Imagen de la Empresa KNQ" class="empresa-logo">
+    </header>
+    <main>
+        <section class="presentation">
+            <h2>Presentación</h2>
+            <p>Bienvenidos a la Empresa KNQ. Somos líderes en el sector de transporte de personal.</p>
+        </section>
+        <nav>
+            <ul>
+                <li><a href="contacto.jsp">Contáctanos</a></li>
+                <li><a href="informacion.jsp">Información</a></li>
+            </ul>
+        </nav>
+    </main>
+    <footer>
+        <p>&copy; 2024 Empresa KNQ</p>
+    </footer>
+</body>
+</html>
