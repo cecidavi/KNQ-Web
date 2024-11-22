8892825dd0fd017a1ab6f741f9470638758ec324
cecidavi <cecireicito_@hotmail.com>
2024-11-22 12:08:51 -0600

    Agragar pagina de informacion.php

--- informacion.php
+++ informacion.php
@@ -4,3 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>KNQ TRANSPORTES DE PERSONAL</title>
+    <link rel="stylesheet" href="css/contacto.css">
+</head>
+<body>
+    <header>
+        <div class="header-content">
+            <img src="asset/knqlogo.jpg" alt="Imagen de la Empresa KNQ" class="empresa-logo">
+            <h1>Quieres saber como contratarnos</h1>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Inicio</a></li>
+                    <li><a href="informacion.php">Información</a></li>
+                    <li><a href="contacto.php">Contáctanos</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
